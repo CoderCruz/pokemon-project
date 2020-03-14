2fc116746ff356a9564fcd90677944f6efdbc22d
Jose Cruz <cruzjosework@gmail.com>
2020-03-14 12:22:15 -0400

    Class component now has constructor, which state is holding my api data after fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      error: null,
+      isLoaded: false,
+      apiData: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon/charizard")
+    .then(res => res.json())
+    .then(
+      (results) => {
+        this.setState({
+          isLoaded: true,
+          apiData: results
+        })
+        console.log(this.state.apiData)
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+    )
+  }
   render() {
     return(
       <div className="App">
